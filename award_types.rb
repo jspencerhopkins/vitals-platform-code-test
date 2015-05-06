f5b143cdbfd4ded7f3563e23f643270eac0d3d2e
jspencerhopkins <jspencerhopkins@gmail.com>
2015-05-05 20:56:44 -0400

    Add BlueCompare class - inherited from NormalItem - tests pass

--- award_types.rb
+++ award_types.rb
@@ -33,3 +33,20 @@ class BlueFirst < NormalItem
   end
 
 end
+
+class BlueCompare < NormalItem
+
+  def update
+    @expires_in -= 1
+    if @expires_in < 0
+      @quality = 0
+    elsif @expires_in < 5
+      self.increase_quality(3)
+    elsif @expires_in < 10
+      self.increase_quality(2)
+    else
+      self.increase_quality(1)
+    end
+  end
+
+end
